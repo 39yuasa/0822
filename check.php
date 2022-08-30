6d2b7ab35e2533c0fb5bee22e7130c18ce876bb9
kousei <21aw0139@jec.ac.jp>
2022-08-30 16:40:29 +0900

    解説完了

--- check.php
+++ check.php
@@ -1,21 +1,36 @@
 <?php
+    // SESSIOnを使えるように宣言
     session_start();
     include('connect.php');
-    if( !isset( $_SESSION['join']) ){ // SESSION内にデータがない場合は再入力させる処理
+    // このページに直接入ってきたときに、$_SESSION['join']が空だったら、newuser（新規登録）の
+
+    if( empty( $_SESSION['join']) ){ 
         header( 'Location: newuser.php');
         exit();
     }
 
-    if(isset($_POST['sub'])):
+    if(!empty($_POST['sub'])):
         // ユーザー登録の処理を行う
+        // SESSIONで持ってきたuser(名前)を$nameという変数に格納する
          $name = $_SESSION['join']['user'];
+        // SESSIONで持ってきたpass(パスワード)を$passという変数に格納する
          $pass =$_SESSION['join']['pass'];
+        // insert intoはデータベースに情報を挿入するときに使う 
+        // mysqli_real_escape_stringはsql用の文字列に変換する
+        // sprintfはフォーマットされた文字で返す。つまりtable上で設定したintやvarcharのフォーマットで返されるのではないかと思います
          $sql = sprintf("INSERT INTO testusers SET name = '%s' , pass ='%s'", mysqli_real_escape_string($com,$name), mysqli_real_escape_string($com,$pass));
-         mysqli_query($com , $sql);
-         unset($_SESSION['join']); //  SESSIONを削除
+        //  形式はこれで返される→INSERT INTO testusers SET name = 'afdasfa' , pass ='asdfassdfa'
+
+        // tableをつなげる、$comに対して$sqlをしてねと俺は思っている
+        mysqli_query($com , $sql);
+        //  SESSIONを削除
+        unset($_SESSION['join']);
+        // データベースとの接続切断
+        mySqli_close($com);
+        //  完了したためページ移動
          header('Location:thanks.html');
     endif;
-    mySqli_close($com);
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,6 +46,7 @@
 <dl>
     <dt>ユーザ名</dt>
         <dd>
+            <!-- htmlspecialchars 特殊文字を HTML エンティティに変換する -->
             <?php
                 echo htmlspecialchars( $_SESSION['join']['user'], ENT_QUOTES, 'UTF-8');
             ?>
--- newuser.php
+++ newuser.php
@@ -1,33 +1,38 @@
 <?php
+//SESSIONを使えるように宣言
 session_start();
 include('connect.php');
-
 $error['user'] = '';
 $error['pass']='';
+//連想配列→｛user:'',pass:''｝の形
 
+// POSTの中身(下のformの内容(sub,user,pass))が入っていたら、trueが入る。なかったらfalseが入る
+// ページに入ったときにはPOSTの中身は空だからfalseが返り、下のHTMLが描画される
 if(!empty($_POST)):
-    // 名前が空だったら
+    // 打ち込まれた「user」エリアが空だったら、error['user']の中身を''(空)から'blank'に変更するよという処理
     if($_POST['user']==''):
         $error['user'] = 'blank';
     endif;
-    // パスワードが空だったら
+      // 打ち込まれた「pass」エリアが空だったら、error['pass']の中身を''(空)から'blank'に変更するよという処理
     if($_POST['pass']==''):
         $error['pass'] = 'blank';
-    // パスワードの文字数が6文字より少なかったら
+    // パスワードの文字数が6文字より少なかったら、error['pass']の中身を''(空)から'length'に変更するよという処理
     elseif(strlen($_POST['pass'])<6):
         $error['pass'] ='length';
     endif;
 
-    $judge = array_filter($error);// error配列の中身を確認
-    // パスワードが空だった場合、{pass: 'blank'}こういう感じで打ち出される
-   
+    // error配列の中身を確認
+    $judge = array_filter($error);
+    // コールバック関数が与えられなかった場合、 $judgeのエントリの中で空の要素はすべて削除される。
+    // blank or length ?  judgeの中にはlengthかblankが入る : (空だったら)errorの中で要素が消され、judgeの中身は空が返る。  
+    
+    //上の処理のあとのjudgeの中身を確認する
+    //judgeの中身が空だったらtrueが返り下の処理が動く,blankかlengthだったらfalseが返り、動かずHTMLが動く
     if(empty($judge)):
         $_SESSION['join']=$_POST;
-        // $_POSTの中には、usernameとpasswordが入っている
-        // $_SESSION['join']['user'] 入力されたユーザ名を確認できる
-        // $_SESSION['join']['pass'] 入力されたパスワードを確認できる
+        // $_POSTの中には、user,password,subの内容が入っている
         header("Location: check.php");
-        // checkへ移動
+        // このページには、もうようがないのでcheckへ移動
 endif;
 endif;
 ?>
@@ -44,6 +49,7 @@ endif;
 <h2>ユーザー登録</h2>
     <form action="" method = 'post'>
     <p>ユーザ名：<input type="text" name="user"></p>
+        <!-- 上の変数、error[user]==''だったら起動しない。登録が押された後、userが空だったらerror[user]の中身がblankに替わり下の処理は起動する -->
         <?php
             if( $error["user"] == "blank" ):
         ?>
@@ -52,6 +58,7 @@ endif;
             endif;
         ?>
         <p>パスワード：<input type="password" name="pass"></p>
+        <!-- 上の変数、error[pass]==''だったら起動しない。登録が押された後、passが6文字以下だったらerror[pass]の中身がlengthに替わり下の処理は起動する -->
         <?php
             if( $error["pass"] == "blank" ):
         ?>
@@ -59,6 +66,7 @@ endif;
         <?php
             endif;
         ?>
+         <!-- 上の変数、error[pass]==''だったら起動しない。登録が押された後、passが6文字以下だったらerror[pass]の中身がlengthに替わり下の処理は起動する -->
         <?php
             if( $error["pass"] == "length" ):
         ?>
